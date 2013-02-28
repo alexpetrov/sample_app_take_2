50409839519a48855ff80c05cd2708ae707591c2
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-03-01 01:08:49 +0400

    refactor new and edit views for users. Fix admin_user filter. Add test, that admin attirbute is not accessible in User

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -85,6 +85,18 @@ describe "Authentication" do
         end
       end
     end
+
+    describe "as non-admin user" do
+      let (:user) { FactoryGirl.create(:user) }
+      let (:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
     
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -102,16 +114,5 @@ describe "Authentication" do
       end
     end
 
-    describe "as non-admin user" do
-      let (:user) { FactoryGirl.create(:user) }
-      let (:non_admin) { FactoryGirl.create(:user) }
-
-      before { sign_in non_admin }
-
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { response.should redirect_to(root_path) }
-      end
-    end
   end
 end
