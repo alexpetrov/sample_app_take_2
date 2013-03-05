fdac0ce861efe0c23c439bc2dd4b76c619b4cdc3
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-03-05 21:42:55 +0400

    addmicroposts model and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  has_many :microposts, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -21,8 +23,6 @@ class User < ActiveRecord::Base
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
-
-  has_secure_password
 
   private
   
