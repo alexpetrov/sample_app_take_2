afa2c3fef4888cfafab5a91e2a5537633eb13f3d
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-04-03 23:48:47 +0400

    reduce some duplication in user profile views

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -69,6 +69,27 @@ describe "User pages" do
         end
       end
     end
+
+    describe "stats" do
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit users_path(user)
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+    end
+
   end
 
   describe "root page" do
