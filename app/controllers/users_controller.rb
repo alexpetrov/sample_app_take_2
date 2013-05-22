82e093e9f26e135847f34709f7788f082892b826
Alexander Petrov <alexpetrov.rb@gmail.com>
2013-05-22 23:22:20 +0400

    Fix synthax with Rubocop recomendations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, 
+  before_filter :signed_in_user,
                 only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: [:destroy]
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @following = @user.followed_users.paginate(page: params[:page])
     @followers = @user.followers.paginate(page: params[:page])
   end
-  
+
   def new
     @user = User.new
   end
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
@@ -36,14 +36,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy 
-    flash[:success] = "User destroyed."
+    User.find(params[:id]).destroy
+    flash[:success] = 'User destroyed.'
     redirect_to users_url
   end
 
   def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       sign_in @user
       redirect_to @user
     else
@@ -52,14 +52,14 @@ class UsersController < ApplicationController
   end
 
   def following
-    @title = "Following"
+    @title = 'Following'
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
-    @title = "Followers"
+    @title = 'Followers'
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
@@ -69,7 +69,7 @@ class UsersController < ApplicationController
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to root_path unless current_user?(@user) 
+    redirect_to root_path unless current_user?(@user)
   end
 
   def admin_user
